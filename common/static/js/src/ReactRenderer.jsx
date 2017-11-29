5160d962da13207aec716acfe5c2797babfb5150
Ari Rizzitano <arizzitano@edx.org>
2017-11-29 17:26:53 -0500

    error handling

--- common/static/js/src/ReactRenderer.jsx
+++ common/static/js/src/ReactRenderer.jsx
@@ -1,24 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class ReactRendererException extends Error {
+  constructor (...args) {
+    super(...args);
+    this.name = 'ReactRendererException';
+    Error.captureStackTrace(this, ReactRendererException);
+  }
+  toString() {
+    return `${this.name}: ${this.message}`;
+  }
+}
+
 export class ReactRenderer {
-  constructor(component, selector, props) {
+  constructor({component, selector, componentName, props={}}) {
+    Object.assign(this, {
+      component,
+      selector,
+      componentName,
+      props,
+    });
+    this.handleModuleErrors();
     this.targetElement = this.getTargetElement(selector);
-    this.props = props;
-    this.component = component;
     this.renderComponent();
+
   }
 
-  ReactRendererException(message) {
-    this.toString = () => {
-      return `ReactRendererException: ${message}`;
+  handleModuleErrors() {
+    if (this.component === null) {
+      throw new ReactRendererException(
+        `Component ${this.componentName} is not defined. Make sure you're
+        using a non-default export statement for the ${this.componentName}
+        class, that ${this.componentName} has an entry point defined
+        within the 'entry' section of webpack.common.config.js, and that the
+        entry point is pointing at the correct file path.`
+      );
     }
   }
 
   getTargetElement(selector) {
     const elementList = document.querySelectorAll(selector);
     if (elementList.length !== 1) {
-      throw new this.ReactRendererException(
+      throw new ReactRendererException(
         `Expected 1 element match for selector "${selector}" but
         received ${elementList.length} matches.`
       );
